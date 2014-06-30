33734def8681de76de299899ff2aacf7e71f69c2
Keiho Sakapon <github@saka-pon.net>
2014-06-30 19:56:16 +0900

    Add HandsContext class

---
+++ NatureUnison/Core/HandsContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NatureUnison
+{
+    public class HandsContext
+    {
+        public static HandsContext Current { get; private set; }
+
+        static HandsContext()
+        {
+            Current = new HandsContext();
+        }
+
+        public event Action<HandFrame?> SingleHandFrame = f => { };
+
+        internal void NotifySingleHandFrame(HandFrame? f)
+        {
+            SingleHandFrame(f);
+        }
+
+        HashSet<HandsTracker> trackers = new HashSet<HandsTracker>();
+
+        public HandsTracker[] GetTrackers()
+        {
+            return trackers.ToArray();
+        }
+
+        public void AddTracker(HandsTracker tracker)
+        {
+            if (tracker == null) throw new ArgumentNullException("tracker");
+
+            tracker.Context = this;
+            trackers.Add(tracker);
+        }
+
+        public void RemoveTracker(HandsTracker tracker)
+        {
+            if (tracker == null) throw new ArgumentNullException("tracker");
+
+            trackers.Remove(tracker);
+            tracker.Context = null;
+        }
+    }
+
+    public struct HandFrame
+    {
+
+    }
+}
---
+++ NatureUnison/Core/HandsTracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NatureUnison
+{
+    public abstract class HandsTracker
+    {
+        internal HandsContext Context { get; set; }
+
+        protected void NotifySingleHandFrame(HandFrame? f)
+        {
+            var context = Context;
+            if (context != null)
+            {
+                context.NotifySingleHandFrame(f);
+            }
+        }
+    }
+}
