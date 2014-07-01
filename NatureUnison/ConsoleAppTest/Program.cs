150d9b0ad584dcdeedc2126e4b2aa6e224323fcd
Keiho Sakapon <github@saka-pon.net>
2014-07-01 17:06:31 +0900

    Add test code

--- NatureUnison/ConsoleAppTest/Program.cs
+++ NatureUnison/ConsoleAppTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NatureUnison;
+using NatureUnison.Leap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,74 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            //IsConnectedTest();
+            //SingleHandFrameTest();
+            //DistanceTest();
+            //PinchReportedTest();
+            DragTest();
+
+            HandsContext.Current.AddTracker(new LeapHandsTracker());
+
+            Console.WriteLine("Press [Enter] key to exit.");
+            Console.ReadLine();
+        }
+
+        static void IsConnectedTest()
+        {
+            LeapContext.Current.AddPropertyChangedHandler("IsConnected", () =>
+            {
+                Console.WriteLine("IsConnected: {0}", LeapContext.Current.IsConnected);
+            });
+        }
+
+        static void SingleHandFrameTest()
+        {
+            HandsContext.Current.SingleHandFrame += f =>
+            {
+                if (f.HasValue)
+                {
+                    Console.WriteLine("{0} {1}", f.Value.Fingers.Length, f.Value.PalmPosition);
+                }
+            };
+        }
+
+        static void DistanceTest()
+        {
+            HandsContext.Current.TwoFingersDistance += (f, d) =>
+            {
+                if (d.HasValue)
+                {
+                    Console.WriteLine("{0:N2}", d.Value);
+                }
+            };
+        }
+
+        static void PinchReportedTest()
+        {
+            HandsContext.Current.PinchReported += (f, b) =>
+            {
+                if (b)
+                {
+                    Console.WriteLine("Pinched {0:HH:mm:ss.fff}", DateTime.Now);
+                }
+            };
+        }
+
+        static void DragTest()
+        {
+            HandsContext.Current.DragStarted += f =>
+            {
+                Console.WriteLine("Drag Started");
+            };
+            HandsContext.Current.Dragged += (f, v) =>
+            {
+                Console.WriteLine(v);
+            };
+            HandsContext.Current.Dropped += (f, v) =>
+            {
+                Console.WriteLine(v.HasValue ? v.Value.ToString() : "Not Tracked");
+                Console.WriteLine("Dropped");
+            };
         }
     }
 }
