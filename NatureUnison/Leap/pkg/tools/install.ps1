d9ad31e956d336743622ef0c4b231dca8d551128
Keiho Sakapon <github@saka-pon.net>
2014-07-14 00:29:56 +0900

    Update Nuspec file

---
+++ NatureUnison/Leap/pkg/tools/install.ps1
@@ -0,0 +1,9 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$LeapItem = $project.ProjectItems.Item("Leap.dll")
+$LeapOutputProp = $LeapItem.Properties.Item("CopyToOutputDirectory")
+$LeapOutputProp.Value = 2
+
+$LeapCSharpItem = $project.ProjectItems.Item("LeapCSharp.dll")
+$LeapCSharpOutputProp = $LeapCSharpItem.Properties.Item("CopyToOutputDirectory")
+$LeapCSharpOutputProp.Value = 2
