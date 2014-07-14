06018162cf1d54fc574ba09a3d3d80ecabc2616e
Keiho Sakapon <github@saka-pon.net>
2014-07-14 12:29:19 +0900

    Add ValueHistory class

---
+++ NatureUnison/Core/ValueHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureUnison
+{
+    public class ValueHistory<T>
+    {
+        public int MaxLength { get; private set; }
+        public Queue<T> History { get; private set; }
+        public bool IsFull { get { return History.Count == MaxLength; } }
+
+        public ValueHistory(int maxLength)
+        {
+            if (maxLength <= 0) throw new ArgumentOutOfRangeException("maxLength", maxLength, "The value must be larger than 0.");
+
+            MaxLength = maxLength;
+            History = new Queue<T>(maxLength);
+        }
+
+        public void UpdateValue(T value)
+        {
+            if (IsFull)
+            {
+                History.Dequeue();
+            }
+            History.Enqueue(value);
+        }
+    }
+
+    public class ShortValueHistory<T>
+    {
+        public T Previous { get; private set; }
+        public T Current { get; private set; }
+
+        public ShortValueHistory(T defaultValue)
+        {
+            Previous = defaultValue;
+            Current = defaultValue;
+        }
+
+        public void UpdateValue(T value)
+        {
+            Previous = Current;
+            Current = value;
+        }
+    }
+}
