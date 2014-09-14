81d1aa8e5c2a677064d96c35be3f52b84d0e8f13
Keiho Sakapon <git@saka-pon.net>
2014-09-14 23:49:01 +0900

    Add shared files

---
+++ Platforms/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("Nature Unison")]
+[assembly: AssemblyCopyright("© 2014 Keiho Sakapon")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
