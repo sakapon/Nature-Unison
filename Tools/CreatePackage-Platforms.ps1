493642fe36c63085c2561e946a5f63046f35215a
Keiho Sakapon <git@saka-pon.net>
2014-09-15 01:54:22 +0900

    Add tool to create package

---
+++ Tools/CreatePackage-Platforms.ps1
@@ -0,0 +1,17 @@
+﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+cd ..\Platforms
+Invoke-Expression ($msbuild + " Platforms.sln /p:Configuration=Release /t:Clean")
+Invoke-Expression ($msbuild + " Platforms.sln /p:Configuration=Release /t:Rebuild")
+
+cd .\Kinect
+.\NuGetPackup.exe
+move *.nupkg ..\..\Published -Force
+
+cd ..\Kinect2
+.\NuGetPackup.exe
+move *.nupkg ..\..\Published -Force
+
+cd ..\Leap
+.\NuGetPackup.exe
+move *.nupkg ..\..\Published -Force
