0d936ca752ef4e3f6574bc92b9733d717a04f782
Keiho Sakapon <git@saka-pon.net>
2014-09-15 13:03:08 +0900

    Update code

--- Platforms/Kinect/KinectContext.cs
+++ Platforms/Kinect/KinectContext.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using KLibrary.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NatureUnison.Platforms.Kinect
 {
-    public class KinectContext
+    public class KinectContext : NotifyBase, IDisposable
     {
+        static Lazy<KinectContext> current = new Lazy<KinectContext>(() => new KinectContext());
+
+        public static KinectContext Current
+        {
+            get { return current.Value; }
+        }
+
+        [DefaultValue(true)]
+        public bool IsAutoDisposing
+        {
+            get { return GetValue<bool>(); }
+            set { SetValue(value); }
+        }
+
+        KinectContext()
+        {
+            AppDomain.CurrentDomain.ProcessExit += (o, e) =>
+            {
+                if (IsAutoDisposing) Dispose();
+            };
+        }
+
+        ~KinectContext()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            try
+            {
+                // TODO: 
+            }
+            catch (Exception)
+            {
+            }
+        }
     }
 }
