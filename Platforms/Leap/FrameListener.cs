c9b4ad1ec150c05d39bb33b061f2047853d72aec
Keiho Sakapon <git@saka-pon.net>
2014-09-15 20:28:25 +0900

    Add classes

---
+++ Platforms/Leap/FrameListener.cs
@@ -0,0 +1,33 @@
+﻿using Leap;
+using System;
+
+namespace NatureUnison.Platforms.Leap
+{
+    class FrameListener : Listener
+    {
+        public event Action<Frame> FrameArrived;
+        public event Action<bool> ConnectionChanged = b => { };
+
+        public override void OnFrame(Controller controller)
+        {
+            var h = FrameArrived;
+            if (h != null)
+            {
+                using (var frame = controller.Frame())
+                {
+                    h(frame);
+                }
+            }
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            ConnectionChanged(true);
+        }
+
+        public override void OnDisconnect(Controller controller)
+        {
+            ConnectionChanged(false);
+        }
+    }
+}
