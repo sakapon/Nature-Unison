da7a669cbef2ecf591652c8eb927ebc650ad0d9e
Keiho Sakapon <git@saka-pon.net>
2014-09-15 21:14:57 +0900

    Add HandTracker class

---
+++ Platforms/Leap/HandTracker.cs
@@ -0,0 +1,66 @@
+﻿using KLibrary.ComponentModel;
+using Leap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Media3D;
+
+namespace NatureUnison.Platforms.Leap
+{
+    public class HandTracker : NotifyBase
+    {
+        public LeapContext Context { get; private set; }
+
+        public Point3D? FingerTipPosition
+        {
+            get { return GetValue<Point3D?>(); }
+            private set { SetValue(value); }
+        }
+
+        public Vector3D? FingerDirection
+        {
+            get { return GetValue<Vector3D?>(); }
+            private set { SetValue(value); }
+        }
+
+        public Quaternion? FingerOrientation
+        {
+            get { return GetValue<Quaternion?>(); }
+            private set { SetValue(value); }
+        }
+
+        public HandTracker()
+        {
+            Context = LeapContext.Current;
+            Context.FrameArrived += Context_FrameArrived;
+        }
+
+        void Context_FrameArrived(Frame frame)
+        {
+            var hand = frame.Hands.Frontmost;
+            if (hand == null || !hand.IsValid)
+            {
+                Clear();
+                return;
+            }
+
+            var p = hand.Pointables.Frontmost;
+            if (p == null || !p.IsValid)
+            {
+                Clear();
+                return;
+            }
+
+            FingerTipPosition = p.StabilizedTipPosition.ToPoint3DOrNull();
+            FingerDirection = p.Direction.ToVector3DOrNull();
+            // TODO: FingerOrientation
+        }
+
+        void Clear()
+        {
+            FingerTipPosition = null;
+            FingerDirection = null;
+            FingerOrientation = null;
+        }
+    }
+}
--- Platforms/Leap/LeapHelper.cs
+++ Platforms/Leap/LeapHelper.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿using Leap;
+using System;
+using System.Windows.Media.Media3D;
 
 namespace NatureUnison.Platforms.Leap
 {
     public static class LeapHelper
     {
+        internal static Point3D ToPoint3D(this Vector v)
+        {
+            if (v == null || !v.IsValid()) throw new ArgumentNullException("v");
+            return new Point3D(v.x, v.y, v.z);
+        }
+
+        internal static Point3D? ToPoint3DOrNull(this Vector v)
+        {
+            return v == null || !v.IsValid()
+                ? default(Point3D?)
+                : new Point3D(v.x, v.y, v.z);
+        }
+
+        internal static Vector3D ToVector3D(this Vector v)
+        {
+            if (v == null || !v.IsValid()) throw new ArgumentNullException("v");
+            return new Vector3D(v.x, v.y, v.z);
+        }
+
+        internal static Vector3D? ToVector3DOrNull(this Vector v)
+        {
+            return v == null || !v.IsValid()
+                ? default(Vector3D?)
+                : new Vector3D(v.x, v.y, v.z);
+        }
     }
 }
